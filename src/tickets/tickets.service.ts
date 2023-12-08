e1fd73376ad77effd44266466e2d151ae14dc9bb
Wander Veloso <wander.veloso@seedz.ag>
2023-12-08 20:04:58 -0300

    feat: add cache in tickets

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Ticket, TicketDocument } from './schema/ticket.schema';
 import { Model } from 'mongoose';
+
+import { Ticket, TicketDocument } from './schema/ticket.schema';
+import { EIGHT_HOURS, TEN_SECONDS } from 'src/utils/redis-times';
 
 export const MESSAGES = {
   ticketCreated: 'ticket created successfully.',
@@ -11,26 +15,42 @@ export const MESSAGES = {
 
 @Injectable()
 export class TicketsService {
-  constructor(@InjectModel(Ticket.name) private ticketModel: Model<TicketDocument>) {}
+  constructor(
+    @InjectModel(Ticket.name) private ticketModel: Model<TicketDocument>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   async create(orderId: string): Promise<{ _id: string; ticketNumber: number }> {
     const ticketNumber = await this.ticketNumberGenerate();
 
-    const { _id } = await this.ticketModel.create({
+    const ticket = await this.ticketModel.create({
       ticketNumber,
       orderId,
       active: true,
     });
 
-    return { _id, ticketNumber };
+    await this.cacheManager.set(ticket._id, ticket, EIGHT_HOURS);
+
+    return { _id: ticket._id, ticketNumber };
   }
 
   async findAll(): Promise<Ticket[]> {
-    return this.ticketModel.find();
+    const ticketsCache: Ticket[] = await this.cacheManager.get('tickets');
+
+    if (!ticketsCache) {
+      const tickets = await this.ticketModel.find();
+
+      await this.cacheManager.set('tickets', tickets, TEN_SECONDS);
+
+      return tickets;
+    }
+
+    return ticketsCache;
   }
 
-  async findOne(id: number): Promise<Ticket> {
-    const ticket = await this.ticketModel.findOne({ _id: id });
+  async findOne(id: string): Promise<Ticket> {
+    const ticket: Ticket =
+      (await this.cacheManager.get(id)) || (await this.ticketModel.findOne({ _id: id }));
 
     if (!ticket) {
       throw new NotFoundException(MESSAGES.ticketNotFound);
@@ -49,6 +69,8 @@ export class TicketsService {
     if (!ticket) {
       throw new NotFoundException(MESSAGES.ticketNotFound);
     }
+
+    await this.cacheManager.del(id);
 
     return { message: MESSAGES.ticketRemoved };
   }
