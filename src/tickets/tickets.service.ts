e8cf55759806c5627e2bb579103ae6ba9b43b809
Wander Veloso <wander.veloso@seedz.ag>
2023-12-12 05:55:19 -0300

    feat: queue implement queue in tickets

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,21 +1,27 @@
 import { Cache } from 'cache-manager';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, forwardRef } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { IQueueMessagePayload, IQueueTicketResponse } from './interface/ticket.interface';
 import { Ticket, TicketDocument } from './schema/ticket.schema';
+import { SqsService } from 'src/aws/sqs.service';
+import { OrdersService } from 'src/orders/orders.service';
 import { EIGHT_HOURS, TEN_SECONDS } from 'src/utils/redis-times';
 import { notFound, removed } from 'src/utils/messages-response';
 
 const TICKET = 'ticket';
 const TICKETS = 'tickets';
+const QUEUE_NAME_FIFO = `${TICKETS}.fifo`;
 
 @Injectable()
 export class TicketsService {
   constructor(
     @InjectModel(Ticket.name) private ticketModel: Model<TicketDocument>,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private sqsService: SqsService,
+    @Inject(forwardRef(() => OrdersService)) private orderService: OrdersService,
   ) {}
 
   async create(orderId: string): Promise<{ _id: string; ticketNumber: number }> {
@@ -28,6 +34,8 @@ export class TicketsService {
     });
 
     await this.cacheManager.set(ticket._id, ticket, EIGHT_HOURS);
+
+    await this.queueSendMessage(orderId, ticketNumber);
 
     return { _id: ticket._id, ticketNumber };
   }
@@ -73,8 +81,41 @@ export class TicketsService {
     return { message: removed(TICKET) };
   }
 
+  async receiveTicketMessage(): Promise<IQueueTicketResponse | string> {
+    const MESSAGE_MAX_NUMBER = 1;
+    const FIRST_INDEX = 0;
+
+    const queueUrl = await this.sqsService.getQueueUrl(QUEUE_NAME_FIFO);
+
+    const messageList = await this.sqsService.receiveMessage(queueUrl, MESSAGE_MAX_NUMBER);
+
+    if (messageList.length) {
+      const message = messageList[FIRST_INDEX];
+
+      await this.sqsService.deleteMessage(queueUrl, message.ReceiptHandle);
+
+      const { orderId, ticketNumber } = JSON.parse(message.Body);
+      const { _id, customerName, description } = await this.orderService.findOne(orderId);
+
+      return { ticketNumber, order: { _id, customerName, description } };
+    }
+
+    return 'There is no ticket in the queue.';
+  }
+
+  private async queueSendMessage(orderId: string, ticketNumber: number): Promise<void> {
+    const payload: IQueueMessagePayload = { orderId, ticketNumber };
+
+    const queueUrl =
+      (await this.sqsService.getQueueUrl(QUEUE_NAME_FIFO)) ||
+      (await this.sqsService.createQueue(QUEUE_NAME_FIFO, true));
+
+    await this.sqsService.sendMessage(queueUrl, JSON.stringify(payload));
+  }
+
   private async ticketNumberGenerate(): Promise<number> {
     const ONE = 1;
+
     const tickets = await this.ticketModel.find();
 
     return tickets.length + ONE;
