20e141b262c907de1980f5ea7bbb43903a5ff072
Wander Veloso <wander.veloso@seedz.ag>
2023-12-12 21:46:23 -0300

    feat: add status schema

---
+++ src/status/schema/status.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+type StatusDocument = Status & Document;
+
+@Schema({ timestamps: true, versionKey: false })
+class Status extends Document {
+  @Prop({ unique: true })
+  orderId: string;
+
+  @Prop()
+  orderCreatedAt: Date;
+
+  @Prop()
+  ticketCreatedAt: Date;
+
+  @Prop()
+  sendQueueMessageAt: Date;
+
+  @Prop()
+  receivedQueueMessageAt: Date;
+
+  @Prop()
+  manufacturingStartedAt: Date;
+
+  @Prop()
+  manufacturingFinishedAt: Date;
+
+  @Prop()
+  customerReceivedAt: Date;
+
+  @Prop()
+  active: boolean;
+}
+
+const StatusSchema = SchemaFactory.createForClass(Status);
+
+export { StatusDocument, Status, StatusSchema };
