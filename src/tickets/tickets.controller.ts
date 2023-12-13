348ad04c3f7411dd79c6341b46e101c03fcdfac7
Wander Veloso <wander.veloso@seedz.ag>
2023-12-13 17:31:10 -0300

    feat: add finishManufacturing in ticketController

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { TicketsService } from './tickets.service';
 import { IQueueTicketResponse } from './interface/ticket.interface';
 
 const SUMMARY_MESSAGE = {
+  finishManufacturing: 'Completes ticket production.',
   queueReceiveMessage: 'Search for a ticket in queue.',
 };
 
@@ -13,9 +14,15 @@ const SUMMARY_MESSAGE = {
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
+  @Post(':id')
+  @ApiOperation({ summary: SUMMARY_MESSAGE.finishManufacturing })
+  finishManufacturing(@Param('id') id: string): Promise<{ message: string }> {
+    return this.ticketsService.finishManufacturing(id);
+  }
+
   @Get()
   @ApiOperation({ summary: SUMMARY_MESSAGE.queueReceiveMessage })
-  queueReceiveMessage(): Promise<IQueueTicketResponse | string> {
+  queueReceiveMessage(): Promise<IQueueTicketResponse | { message: string }> {
     return this.ticketsService.queueReceiveMessage();
   }
 }
