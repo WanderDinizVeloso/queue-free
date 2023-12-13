c271c2f769bd450781e29d0b5f2a1aaab18d390b
Wander Veloso <wander.veloso@seedz.ag>
2023-12-12 21:45:54 -0300

    feat: add status module

---
+++ src/status/status.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { Status, StatusSchema } from './schema/status.schema';
+import { StatusController } from './status.controller';
+import { StatusService } from './status.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Status.name, schema: StatusSchema }])],
+  controllers: [StatusController],
+  providers: [StatusService],
+  exports: [StatusService],
+})
+export class StatusModule {}
